43090e6bcb15ea7cdb13212110043001f170bc96
Jack Williams <jackw667@gmail.com>
2019-02-10 23:13:13 -0500

    Create board class

---
+++ Hive/board.cpp
@@ -0,0 +1,26 @@
+#include "pch.h"
+#include "board.h"
+#include <stdlib.h>
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+
+
+using std::cout;
+using std::endl;
+using std::string;
+
+board::board()
+{
+
+}
+
+
+board::~board()
+{
+}
+
+void board::printBoard()
+{
+
+}
