aff76c2abfa2b07d37f4e54e46580c8077b14230
Jack Williams <jackw667@gmail.com>
2019-02-10 23:15:11 -0500

    test printing hexagons to cmd line

--- Hive/Hive.cpp
+++ Hive/Hive.cpp
@@ -1,21 +1,34 @@
 // Hive.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include "pch.h"
 #include <iostream>
+#include <stdlib.h>
+#include <string>
+
+using std::cout;
+using std::endl;
+using std::string;
 
 int main()
 {
-    std::cout << "Welcome to Hive!\n"; 
+	cout << "Welcome to Hive!\n";
+
+	string white = "WHITE";
+	string black = "BLACK";
+
+	cout << "  ____" << endl;
+	cout << " /    \\" << endl;
+	cout << "/" << black << " \\____" << endl;
+
+	for (int i = 0; i < 5; i++) {
+		int c = rand() % 2;
+		string color = c == 0 ? white : black;
+		cout << "\\ Ant  /    \\" << endl;
+		cout << " \\____/" << color << " \\" << endl;
+		cout << " /    \\ Ant  /" << endl;
+		cout << "/" << color << " \\____/" << endl;
+	}
+
+	cout << "\\ Ant  /" << endl;
+	cout << " \\____/" << endl;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
