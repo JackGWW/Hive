2634ebdbfce00d58eac2a4af8e4f3c881b643581
Jack Williams <jackw667@gmail.com>
2019-02-13 00:17:40 -0500

    Add hashmap to store pieces in board

--- Hive/board.cpp
+++ Hive/board.cpp
@@ -1,26 +1,81 @@
 #include "pch.h"
 #include "board.h"
+#include "piece.h"
 #include <stdlib.h>
 #include <iostream>
 #include <stdlib.h>
 #include <string>
+#include <unordered_map>
 
 
 using std::cout;
 using std::endl;
 using std::string;
+using std::unordered_map;
 
-board::board()
+Location::Location(int _x, int _y, int _z) : x(_x), y(_y), z(_z)
 {
 
 }
 
+string Location::to_string()
+{
+	string str = "";
+	str += "x: " + std::to_string(x) + "  ";
+	str += "y: " + std::to_string(y) + "  ";
+	str += "z: " + std::to_string(z);
 
-board::~board()
+	return str;
+}
+
+bool operator==(const Location& x, const Location& y)
+{
+	return x.x == y.x && x.y == y.y;
+}
+
+
+
+bool keyExists(unordered_map<Location, Piece> m, Location key)
+{
+	if (m.find(key) == m.end())
+		return false;
+
+	return true;
+}
+
+
+Board::Board()
+{
+	unordered_map<Location, Piece> pieces;
+}
+
+
+Board::~Board()
 {
 }
 
-void board::printBoard()
+void Board::print()
 {
+	int maxX = INT_MIN, maxY = INT_MIN;
+	int minX = INT_MAX, minY = INT_MAX;
 
+	for (auto& it : pieces) {
+		Location l = it.first;
+
+		if (l.x > maxX) maxX = l.x;
+		if (l.y > maxY) maxY = l.y;
+		if (l.x < minX) minX = l.x;
+		if (l.y < minY) minY = l.y;
+	}
+	cout << maxX << ',' << maxY << ' ' << minX << ',' << minY << endl;
+}
+
+void Board::add(Location l, Piece p)
+{
+	if (keyExists(pieces, l))
+	{
+		throw std::invalid_argument("There is already a piece in location: " + l.to_string());
+	}
+
+	pieces.insert(std::make_pair(l, p));
 }
--- Hive/board.h
+++ Hive/board.h
@@ -1,9 +1,47 @@
 #pragma once
-class board
+#include <unordered_map>
+#include "piece.h"
+
+using std::unordered_map;
+
+
+
+struct Location
 {
+	int x, y, z;
+	Location(int, int, int);
+	string to_string();
+};
+
+bool operator==(const Location& x, const Location& y);
+
+
+namespace std {
+
+	template <>
+	struct hash<Location>
+	{
+		std::size_t operator()(const Location& l) const
+		{
+			using std::size_t;
+			using std::hash;
+			using std::string;
+
+			//x & y range from -50 to 50, z ranges from 0-10
+			return l.x + l.z * 10 + l.y * 100;
+		}
+	};
+}
+
+
+class Board
+{
+private:
+	unordered_map<Location, Piece> pieces;
 public:
-	board();
-	~board();
-	void printBoard();
+	Board();
+	~Board();
+	void print();
+	void add(Location, Piece);
 };
 
