1db78d92a81dcebf72c93bfb40cff41bbdcbd070
Jack Williams <jackgww@gmail.com>
2019-03-03 17:06:43 -0500

    add spider piece

---
+++ Hive/spider.cpp
@@ -0,0 +1,20 @@
+#include "spider.h"
+
+
+
+Spider::Spider(string _color)
+{
+	color = _color;
+	name = "SPIDR";
+}
+
+
+Spider::~Spider()
+{
+}
+
+vector<Location> Spider::moves(Board, Location)
+{
+	
+	return vector<Location>();
+}
