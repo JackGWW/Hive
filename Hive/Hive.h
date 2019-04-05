d80097dea720fa8c637a3b0a84fce193e356eab5
Jack Williams <jackw667@gmail.com>
2019-04-05 19:12:11 -0400

    initial commit

---
+++ Hive/Hive.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "board.h"
+#include "pch.h"
+#include "piece.h"
+#include "Grasshopper.h"
+#include "board.h"
+#include <vector>
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+
+using std::vector;
+
+void printLocations(vector<Location>);
