3a34777b2ee0d49e25a3eddcde35f1ec34008c12
Jack Williams <jackw667@gmail.com>
2019-04-21 15:16:55 -0400

    Add game class

--- Hive/Hive.cpp
+++ Hive/Hive.cpp
@@ -1,14 +1,23 @@
 // Hive.cpp : This file contains the 'main' function. Program execution begins and ends there.
 
 #include "Hive.h"
+#include <unordered_map>
+#include <vector>
 
+using std::unordered_map;
 using std::cout;
 using std::endl;
+using std::vector;
 using std::string;
 
 // TODO add in proper public/private usage
 // TODO add in const usage
 // TODO add virtual classes and virtual desctructor to piece
+
+string white = "white";
+string black = "black";
+vector<string> colors{ white, black };
+
 
 int main()
 {
@@ -27,13 +36,15 @@ int main()
 
 	printLocations(testv);
 
+	Game game = Game("Jack", "Thomas");
+
 	cout << "DONE" << endl;
 }
 
 void printLocations(vector<Location> locations)
 {
 	Board b;
-	Piece testPiece = Piece("test", "TEST");
+	Piece testPiece = Piece("test", "SPIDER");
 
 	//Add pieces to board
 	for (Location l : locations) {
@@ -48,4 +59,41 @@ void printLocations(vector<Location> locations)
 	}
 
 	b.print();
+}
+
+Game::Game(string p1, string p2)
+{
+	names.insert({ white, p1 });
+	names.insert({ black, p2 });
+
+	addStartingPieces();
+
+	board.add(Location(1, 1), Ant(white));
+	board.print();
+}
+
+Game::~Game()
+{
+}
+
+
+void Game::addStartingPieces()
+{
+	for (string color : colors) {
+		unusedPieces[color].push_back(Queen(color));
+
+		unusedPieces[color].push_back(Beetle(color));
+		unusedPieces[color].push_back(Beetle(color));
+		
+		unusedPieces[color].push_back(Spider(color));
+		unusedPieces[color].push_back(Spider(color));
+		
+		unusedPieces[color].push_back(Grasshopper(color));
+		unusedPieces[color].push_back(Grasshopper(color));
+		unusedPieces[color].push_back(Grasshopper(color));
+		
+		unusedPieces[color].push_back(Ant(color));
+		unusedPieces[color].push_back(Ant(color));
+		unusedPieces[color].push_back(Ant(color));
+	}
 }
--- Hive/Hive.h
+++ Hive/Hive.h
@@ -4,6 +4,10 @@
 #include "pch.h"
 #include "piece.h"
 #include "Grasshopper.h"
+#include "ant.h"
+#include "spider.h"
+#include "beetle.h"
+#include "queen.h"
 #include "board.h"
 #include <vector>
 #include <iostream>
@@ -13,3 +17,16 @@
 using std::vector;
 
 void printLocations(vector<Location>);
+
+
+class Game
+{
+private:
+	unordered_map<string, string> names;
+	unordered_map<string, vector<Piece>> unusedPieces;
+	Board board;
+public:
+	Game(string, string);
+	~Game();
+	void addStartingPieces();
+};
